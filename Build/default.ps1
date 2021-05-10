7c80795217d679cc199371c6e89d6a0c51f0767d
Przemysław Piwowarczyk <przemek.piwowarczyk@gmail.com>
2021-05-11 01:10:57 +0200

    Add unit test projects, add build for unit tests

--- Build/default.ps1
+++ Build/default.ps1
@@ -1,4 +1,6 @@
-﻿properties {
+﻿Include ".\helpers.ps1"
+
+properties {
 	$testMessage = 'Executed Test!'
 	$cleanMessage = 'Executed Clean!'
 
@@ -6,8 +8,23 @@
 	$outputDirectory= "$solutionDirectory\.build"
 	$temporaryOutputDirectory = "$outputDirectory\temp"
 
+	$publishedNUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedNUnitTests"
+	$publishedxUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedxUnitTests"
+	$publishedMSTestTestsDirectory = "$temporaryOutputDirectory\_PublishedMSTestTests"
+
+	$testResultDirectory = "$outputDirectory\TestResults"
+	$NUnitTestResultDirectory = "$testResultDirectory\NUnit"
+	$xUnitTestResultDirectory = "$testResultDirectory\xUnit"
+	$MSTestTestResultDirectory = "$testResultDirectory\MSTest"
+
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
+
+	$packagePath = "$solutionDirectory\packages"
+	$NUnitExe = (Find-PackagePath $packagePath "NUnit.Runners") + "\Tools\nunit-console-x86.exe"
+	$xUnitExe = (Find-PackagePath $packagePath "xUnit.Runner.Console") + "\Tools\xunit-console.exe"
+	$vsTestExe = (Get-ChildItem ("C:\Program Files (x86)\Microsoft Visual Studio*\Common7\IDE\CommonExtenstions\Microsoft\TestWindow\vstest.console.exe")).FullName | 
+	Sort-Object $_ | select -last 1
 }
 
 task default -depends Test
@@ -23,6 +40,13 @@ task Init `
 
 	Assert ("x86", "x64", "Any CPU" -contains $buildPlatform) `
 		   "Invalid build platform '$buildPlatform'. Valid values are 'x86', 'x64' or 'Any CPU'"
+
+	Write-Host "Checking that all required tools are available"
+
+	#Check that all tools are available
+	Assert (Test-Path $NUnitExe) "NUnit Console could not be found"
+	Assert (Test-Path $xUnitExe) "xUnit Console could not be found"
+	Assert (Test-Path $vsTestExe) "VSTest Console could not be found"
 
 	# Remove previous build results
 	if (Test-Path $outputDirectory) 
@@ -47,6 +71,57 @@ task Compile `
 	#msbuild $SolutionFile "/p:Configuration=$buildConfiguration;Platform=$buildPlatform;OutDir=$temporaryOutputDirectory"
 
 	Exec { msbuild $SolutionFile "/p:Configuration=$buildConfiguration;Platform=$buildPlatform;OutDir=$temporaryOutputDirectory" }
+}
+
+task TestNUnit `
+	-depends Compile `
+	-description "Run NUnit tests" ` 
+	-precondition { return Test-Path $publishedNUnitTestsDirectory} `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "NUnit" `
+									-publishedTestsDirectory $publishedNUnitTestsDirectory `
+								    -testResultsDirectory $NUnitTestResultDirectory
+
+	Exec { &$NUnitExe $testAssemblies /xml:$NUnitTestResultDirectory\NUnit.xml /nologo /noshadow }
+}
+
+task TestXUnit `
+	-depends Compile `
+	-description "Run NUnit tests"` 
+	-precondition { return Test-Path $publishedxUnitTestsDirectory} `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "xUnit" `
+									-publishedTestsDirectory $publishedxUnitTestsDirectory `
+								    -testResultsDirectory $xUnitTestResultDirectory
+
+	Exec { &$xUnitExe $testAssemblies -xml $NUnitTestResultDirectory\xUnit.xml -nologo -noshadow }
+}
+
+task TestMSTest `
+	-depends Compile `
+	-description "Run NUnit tests"` 
+	-precondition { return Test-Path $publishedMSTestTestsDirectory} `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "MSTest" `
+									-publishedTestsDirectory $publishedMSTestTestsDirectory `
+								    -testResultsDirectory $MSTestTestResultDirectory
+
+	#vstest console doesn't have any option to change the output directory so we need to change working directory
+	Push-Location $MSTestTestResultDirectory
+	Exec { &$vsTestExe $testAssemblies /Logger:trx }
+	Pop-Location 
+
+	#move the .trc file back to $MSTestTestResultDirectory
+	Move-Item -path $MSTestTestResultDirectory\TestResults\*.trx -Destination $MSTestTestResultDirectory\MSTest.trx
+
+	Remove-Item $MSTestTestResultDirectory\TestResults
+}
+
+task Test `
+	-depends Compile, TestNUnit, TestXUnit, TestMSTest `
+	-description "Run unit tests"` 
+{
+
 }
 
 task Test -depends Compile, Clean -description "Run unit tests" { 
---
+++ Build/helpers.ps1
@@ -0,0 +1,46 @@
+﻿function Find-PackagePath
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0,Mandatory=1)]$packagePath,
+		[Parameter(Position=1,Mandatory=1)]$packageName
+	)
+
+	return (Get-ChildItem ($packagePath + "\" + $packageName + "*").FullName | Sort-Object $_ | select -Last 1
+}
+
+function Prepare-Tests
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0,Mandatory=1)]$testRunnerName,
+		[Parameter(Position=1,Mandatory=1)]$publishedTestsDirectory,
+		[Parameter(Position=2,Mandatory=1)]$testResultDirectory
+	)
+
+	$projects = Get-ChildItem $publishedTestsDirectory
+
+	if ($projects.Count -eq 1)
+	{
+		Write-Host "1 $testRunnerName project has been found"
+	}
+	else 
+	{
+		Write-Host $projects.Count " $testRunnerName projects have been found"
+	}
+
+	Write-Host ($projects | Select $_.Name)
+
+	#Create the test result directory if needed
+	if(!(Test-Path $testResultDirectory))
+	{
+		Write-Host "Creating test result directory located at $testResultDirectory"
+		mkdir $testResultDirectory | Out-Null
+	}
+
+	$testAssembliesPaths = $projects | ForEach-Object {$_.FullName + "\" + $_.Name + ".dll"}
+
+	$testAssemblies = [string]::Join(" ", $testAssembliesPaths)
+
+	return $testAssemblies
+}
